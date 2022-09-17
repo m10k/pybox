af9bea85e6c8ab759c42f8071591cc4ed71ea5b1
Matthias Kruk <m@m10k.eu>
2022-09-17 16:39:45 +0900

    setup.py: Make library installable via python3-setuptools
    This commit adds a minimal setup.py file, allowing the library to
    be installed via python3's setuptools.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='pybox',
+      version='0.1',
+      description='Toolbox module for Python',
+      author='Matthias Kruk',
+      author_email='m@m10k.eu',
+      url='https://github.com/m10k/pybox',
+      packages=['pybox']
+      )
